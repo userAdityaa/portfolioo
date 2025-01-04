aeb609a6e551b2822d3e4b7f0f16ca01e9cb56f3
userAdityaa <aditya.chaudhary1558@gmail.com>
2025-01-05 04:57:16 +0545

    added the name role and the profile icons

--- app/page.tsx
+++ app/page.tsx
@@ -34,6 +34,30 @@ export default function Home() {
           width={175}
           className="rounded-lg"
         />
+        <div className="flex flex-col pt-14">
+          <div className="flex items-center gap-3">
+            <h1 className="text-zinc-300 font-bold text-[25px]">Aditya Chaudhary</h1>
+            <span className="bg-green-900/30 text-green-400 px-2 py-0.5 rounded text-sm">Available</span>
+          </div>
+          <p className="text-gray-300">Software Engineer</p>
+          <div className="flex gap-3 mt-1">
+            <a href="#" className="bg-black w-9 h-9 rounded-lg flex items-center justify-center hover:bg-zinc-900 transition-colors shadow-lg">
+              <Image src="/github.svg" alt="GitHub" width={24} height={24} className="invert" />
+            </a>
+            <a href="#" className="bg-black w-9 h-9 rounded-lg flex items-center justify-center hover:bg-zinc-900 transition-colors shadow-lg">
+              <Image src="/twitter.svg" alt="Twitter" width={24} height={24} className="invert" />
+            </a>
+            <a href="#" className="bg-black w-9 h-9 rounded-lg flex items-center justify-center hover:bg-zinc-900 transition-colors shadow-lg">
+              <Image src="/discord.svg" alt="Discord" width={24} height={24} className="invert" />
+            </a>
+            <a href="#" className="bg-black w-9 h-9 rounded-lg flex items-center justify-center hover:bg-zinc-900 transition-colors shadow-lg">
+              <Image src="/linkedin.svg" alt="LinkedIn" width={24} height={24} className="invert" />
+            </a>
+            <a href="#" className="bg-black w-9 h-9 rounded-lg flex items-center justify-center hover:bg-zinc-900 transition-colors shadow-lg">
+              <Image src="/leetcode.svg" alt="leetcode" width={24} height={24} className="invert" />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
