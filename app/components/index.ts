632f00b56fd1e7add88832a5ed410c14d30c381f
userAdityaa <aditya.chaudhary1558@gmail.com>
2025-01-06 02:48:13 +0545

    added all the import path for components

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,5 +1,11 @@
 import SkillsSection from "./SkillSection";
+import ProjectsSection from "./ProjectCard";
+import ContactAndFooter from "./Footer";
+import ProfileImageSwitcher from "./ProfileImage";
 
 export { 
     SkillsSection,
+    ProjectsSection,
+    ContactAndFooter,
+    ProfileImageSwitcher
 }
