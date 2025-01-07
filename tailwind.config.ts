5329524f267fcbf59d4c0ca84e7862a86e364575
userAdityaa <aditya.chaudhary1558@gmail.com>
2025-01-07 21:04:30 +0545

    tailwind_config: added the necessarry tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      screens: {
+        'max-phone': { max: '600px' },
+    }
     },
   },
   plugins: [],
